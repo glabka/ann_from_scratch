d9a88cf4a452417c94b64254aba4bf80c0ae025f
glabka <glabka333@gmail.com>
2022-09-18 21:26:21 +0200

    customizable neural network - error deltas probably working

--- customizable_nn/custom_nn.py
+++ customizable_nn/custom_nn.py
@@ -22,32 +22,59 @@ class CustomNN:
             self.weights_list = [[[2, 0, 0], [0, 2, 0], [0, 0, 2]], [[-3, 0, 0], [0, -3, 0], [0, 0, -3]]] # TODO -delete -  debug
             self.weights_list = [[[1, 0], [0, 1], [1, 1]], [[2, 0, 0], [0, -1, 0], [0, 0, 0]]]# TODO -delete -  debug
 
-    def mini_batch_learn(self, inputs, targets):
-        preds, act_o = self.calc_preds(inputs)
-        cost = self.cost_fun(act_o, targets)
+    def mini_batch_learn(self, inputs, targets_l):
+        preds_l, act_o_l = list(zip(*[self.calc_preds(input) for input in inputs])) # _l as list for each input
+        cost = self.cost_fun(act_o_l, targets_l)
         print(f"cost: {cost}")
-        print(f"len(preds): {len(preds)}, len(preds[0]): {len(preds[0])}, len(preds[0][0]): {len(preds[0][0])}, preds: {preds}")
-        errors_d = self.calc_error_d(preds, targets)
-        print(f"len(errors_d): {len(errors_d)}, len(errors_d[0]): {len(errors_d[0])}, len(errors_d[0][0]): {len(errors_d[0][0])}, preds: {preds}")
-        return preds # debug
+        print(f"len(preds_l) = len(batches) : {len(preds_l)}, len(preds_l[0] = len(numOfLayers) - 1: {len(preds_l[0])}, preds_l: {preds_l}")
+        errors_d_l = [self.calc_error_d(preds, targets) for preds, targets in zip(preds_l, targets_l)]
+        print(f"len(errors_d_l) = len(batch): {len(errors_d_l)}, len(errors_d[0]) = len(layers) - 1: {len(errors_d_l[0])}, errors_d_l: {errors_d_l}")
+        # gradients - list for every batch' input
+        gradients_l = [gradients(errors_d_l, preds_l.inset(0, inputs))]
+
+
+    def gradients(self, errors_d_l, preds_and_inp):
+        return None # TODO
+
 
     # inputs - column vector.
     # return tuple contining list of prediction and of all layers as column vectors and list of activations from last layer
-    def calc_preds(self, inputs):
+    def calc_preds(self, input):
         preds = []
         acts = []
         print(f"len(weights_list): {len(self.weights_list)}")
         for i in range(len(self.weights_list)):
             if i == 0:
-                print(f"input: {inputs}, weights: {self.weights_list[i]}")
-                preds.append(self.matrix_multiplication(self.weights_list[i], inputs))
+                print(f"input: {input}, weights: {self.weights_list[i]}")
+                preds.append(self.matrix_mult_vector(self.weights_list[i], input))
             else :
                 print(f"input: {preds[i - 1]}, weights: {self.weights_list[i]}")
-                preds.append(self.matrix_multiplication(self.weights_list[i], acts[i - 1]))
-            acts.append(self.layers_act_funcs[i](CustomNN.transpose(preds[i])[0])) # [0] beacause everything is a matrix so vector is nestead in extra list
+                preds.append(self.matrix_mult_vector(self.weights_list[i], acts[i - 1]))
+            acts.append(self.layers_act_funcs[i](preds[i]))
         return preds, acts[-1]
 
+    # a x b
+    # A - column vector -> represented by list of numbers
+    # b - row vector -> represented by list of numbers
+    # returns matrix (list of lists of numbers)
+    @staticmethod
+    def vector_mat_mult_vector(a, b):
+        tmp_dbg = [[a_num * b_num for b_num in b] for a_num in a]
+        return tmp_dbg
+
+    # A x b
+    # A - matrix - has to be a list of lists of numbers
+    # b - column vector - represented by list of numbers
+    # returns list of numbers
+    @staticmethod
+    def matrix_mult_vector(a, b):
+        tmp_dbg = [sum([a_num * b_num for a_num, b_num in zip(a_row, b)]) for a_row in a]
+        return tmp_dbg
+
     # A x B
+    # A - matrix (both dimensions are greater than 1)
+    # B - matrix (both dimensions are greater than 1)
+    # returns matrix
     @staticmethod
     def matrix_multiplication(a, b):
         tmp_dbg = [[sum([a_num * b_num for a_num, b_num in zip(a_row, b_column)]) for a_row in a] for b_column in CustomNN.transpose(b)]
@@ -58,26 +85,20 @@ class CustomNN:
     @staticmethod
     def transpose(m):
         print(f"transposing: {m}")
-        # if all(type(i) in [float, int] for i in m):
-        #     return [[item] for item in m]
-        # elif len(m) == 1 and isinstance(m[0], list):
-        #     return [[[item] for item in m[0]]]
-        # else:
-        return [list(a) for a in zip(*m)]
+        return list(zip(*m))
+        # return [list(a) for a in zip(*m)]
 
     # preds - list of predictions of every layer
     # targets - list of targets
-    # returns list of column vectors representing errors derivatives/deltas for every layer
+    # returns list of column vectors representing errors derivatives/deltas for every layer with exception of the first
     def calc_error_d(self, preds, targets):
         errors_d = []
         for i in range(len(preds) - 1, -1, -1):
-            if i == len(preds): # last layer
-                # errors_d.append()
-                error_o_d = [[self.delta_calc_func[i](a, t) for a, t in zip(ac, tg)] for ac, tg in zip(self.layers_act_funcs[i](preds[i]), targets)]
+            if i == len(preds) - 1: # last layer
+                error_o_d = [self.delta_calc_funcs[i](a, t) for a, t in zip(self.layers_act_funcs[i](preds[i]), targets)]
                 errors_d.append(error_o_d)
             else:
-                # errors_d_i = [[sum([w * d for w, d in zip(w_r, err_d)]) * activationd_d(p) for w_r, p in zip(w_h_o_T, pr)] for err_d, pr in zip(error_o_d, pred_h)]
-                # error_d_h = w_h_o_T matrix multiplying errors_o_d elementwise multiplied with ReLU_d
-                errors_d_i = [[[[scal * der for scal in row] for der in self.delta_calc_funcs[i](p)] for p in preds[i]] for row in CustomNN.matrix_multiplication(CustomNN.transpose(self.weights_list[i + 1]), errors_d_i[i + 1])]
-                errors_d.insert(0, errors_d_i)
-        errors_d
+                back_error = CustomNN.matrix_mult_vector(CustomNN.transpose(self.weights_list[i + 1]), errors_d[-1]) # "back error"
+                errors_d_h = [err * self.delta_calc_funcs[i](p) for err, p in zip(back_error, preds[i])]
+                errors_d.insert(0, errors_d_h)
+        return errors_d
--- customizable_nn/flower_classifier.py
+++ customizable_nn/flower_classifier.py
@@ -18,7 +18,7 @@ def log_loss(act, target):
     return -target * math.log(act) - (1 - target) * math.log(1 - act)
 
 def cost_fun(act_o, targets):
-    return sum([sum([log_loss(a, t) for a, t in zip(ac, tg)]) for ac, tg in zip(act_o, targets)]) / len(targets)
+    return sum([sum([log_loss(a, t) for a, t in zip(act, tg)]) for act, tg in zip(act_o, targets)]) / len(targets)
 
 def diff(a, t):
     return a - t
@@ -30,10 +30,14 @@ def diff(a, t):
 # print(CustomNN.transpose(CustomNN.transpose([[0], [1], [2]])))
 # print(CustomNN.transpose([[1, 2, 3], [4, 5, 6]]))
 
+# print(CustomNN.vector_mat_mult_vector([1, 2, 3, 4], [1, -1]))
+# print(CustomNN.matrix_mult_vector([[1, 2, 3, 4]], [1, -1, 0, 0]))
+# print(CustomNN.matrix_mult_vector([[1, 2, 3, 4], [10, 20, 30, 40]], [1, -1, 0, 0]))
+
 # nn = CustomNN(0.1, cost_fun, [2, 4, 3], [None, relu, soft_max], [diff, relu_d, diff])
 
 nn = CustomNN(0.1, cost_fun, [2, 4, 3], [relu, soft_max], [relu_d, diff])
-print(nn.mini_batch_learn([[1], [-1]], CustomNN.transpose([[2, 1, 0]])))
+print(nn.mini_batch_learn([[1, -1], [2, -2]], [[2, 1, 0], [4, 2, 0]]))
 
 # nn = CustomNN(0, None, [3, 5, 2], [], [])
 # print(nn.calc_preds([[1], [-1], [0]]))
